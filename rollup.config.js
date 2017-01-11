664e7d99c0ed6979a3b121326c095c7358506c17
十界 <sonne@asaki.me>
2017-01-12 01:46:32 +0800

    update uglify config

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ export default {
     require('rollup-plugin-babel')({
       exclude: 'node_modules/**'
     }),
+    require('rollup-plugin-uglify')(),
     require('rollup-plugin-node-resolve')({
       jsnext: true,
       main: true,
@@ -13,8 +14,7 @@ export default {
     require('rollup-plugin-commonjs')(),
     require('rollup-plugin-replace')({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    }),
-    (process.env.NODE_ENV === 'production' && require('rollup-plugin-uglify')())
+    })
   ],
   format: 'umd',
   moduleName: 'SvelteRouter',
