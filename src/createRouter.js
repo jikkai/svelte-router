7b558f101e0cfbb8d32f83c9514f9a2dd3097253
Jikkai Xiao <sonne@asaki.me>
2017-01-12 11:30:59 +0800

    bug fixed

--- src/createRouter.js
+++ src/createRouter.js
@@ -1,62 +1,63 @@
 import RouteParser from 'route-parser'
 import history from './history'
 
-const createRouter = (routes) => {
-  let content
-  let unlisten
-  let target
+const createRouter = (options) => {
+  let _content
+  let _unlisten
+  let _target
 
-  const createRouteBehavior = (route) => {
-    if (typeof route === 'function') {
+  const createRouteBehavior = (Route) => {
+    if (typeof Route === 'function') {
       return (options) => {
-        // eslint-disable-next-line new-cap
-        content = new route(options)
-        return content
+        _content = new Route(options)
+        return _content
       }
     }
 
-    if (typeof route === 'object') {
-      if (route.redirect) {
-        return () => history.push(route.redirect)
+    if (typeof Route === 'object') {
+      if (Route.redirect) {
+        return () => history.push(Route.redirect)
       }
     }
 
-    if (typeof route === 'string') {
-      return () => history.push(route)
+    if (typeof Route === 'string') {
+      return () => history.push(Route)
     }
 
     return () => {}
   }
 
-  const routeData = Object.keys(routes)
-    .map(key => [key, routes[key]])
+  const routeData = Object.keys(options)
+    .map(key => [key, options[key]])
     .map(([key, value]) => ({
       route: new RouteParser(key),
       behavior: createRouteBehavior(value)
     }))
 
-  const handleRouteChange = (location) => {
-    if (content && content.teardown) content.teardown()
+  const handleRouteChange = () => {
+    if (_content && _content._fragment) _content.teardown()
 
     for (let i = 0; i < routeData.length; i++) {
-      const data = routeData[i].route.match(location.pathname)
+      const data = routeData[i].route.match(history.location.pathname)
       if (data) {
-        routeData[i].behavior({ target, data })
+        routeData[i].behavior({ target: _target, data })
         break
       }
     }
   }
 
   return {
-    start (location, targetElement) {
-      target = targetElement
-      unlisten = history.listen(handleRouteChange)
-      handleRouteChange(history.location)
+    start (targetElement) {
+      _target = typeof targetElement === 'string'
+        ? document.querySelector(targetElement) : targetElement
+
+      _unlisten = history.listen(handleRouteChange)
+      handleRouteChange()
     },
     teardown () {
-      if (unlisten) {
-        unlisten()
-        unlisten = undefined
+      if (_unlisten) {
+        _unlisten()
+        _unlisten = undefined
       }
     }
   }
